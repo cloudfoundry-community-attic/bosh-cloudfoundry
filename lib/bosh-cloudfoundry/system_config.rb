472c7cca1e80ec2adc46e18523ff5b40c84f0b8f
Dr Nic Williams <drnicwilliams@gmail.com>
2013-01-02 08:54:11 -0800

    Compress class/module definition

--- lib/bosh-cloudfoundry/system_config.rb
+++ lib/bosh-cloudfoundry/system_config.rb
@@ -1,24 +1,22 @@
 # Copyright (c) 2012-2013 Stark & Wayne, LLC
 
-module Bosh; end
+module Bosh; module CloudFoundry; end; end
 
-module Bosh::CloudFoundry
-  class SystemConfig < Bosh::Cli::Config
+class Bosh::CloudFoundry::SystemConfig < Bosh::Cli::Config
 
-    [
-      :system_name,      # e.g. production
-      :system_dir,       # e.g. /var/vcap/store/systems/production
-      :release_name,     # e.g. 'appcloud'
-      :stemcell_version, # e.g. '0.6.7'
-      :runtimes,         # e.g. { "ruby18" => false, "ruby19" => true }
-    ].each do |attr|
-      define_method attr do
-        read(attr, false)
-      end
+  [
+    :system_name,      # e.g. production
+    :system_dir,       # e.g. /var/vcap/store/systems/production
+    :release_name,     # e.g. 'appcloud'
+    :stemcell_version, # e.g. '0.6.7'
+    :runtimes,         # e.g. { "ruby18" => false, "ruby19" => true }
+  ].each do |attr|
+    define_method attr do
+      read(attr, false)
+    end
 
-      define_method "#{attr}=" do |value|
-        write_global(attr, value)
-      end
+    define_method "#{attr}=" do |value|
+      write_global(attr, value)
     end
   end
 end
