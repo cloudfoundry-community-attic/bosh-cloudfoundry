3fd6fa782d7fb8e024acaf5ed0cb7d85f702dfbf
Dr Nic Williams <drnicwilliams@gmail.com>
2013-07-09 20:30:02 -0700

    rspec setup

--- Rakefile
+++ Rakefile
@@ -1,12 +1,22 @@
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __FILE__)
+
+require "rubygems"
+require "bundler"
+Bundler.setup(:default, :test, :development)
+
 require "bundler/gem_tasks"
 
+require "rake/dsl_definition"
+require "rake"
+require "rspec/core/rake_task"
+
 bosh_release = "cf-release"
-target_bosh_release_path = File.expand_path("../bosh_release", __FILE__)
 
 namespace :bosh do
   namespace :release do
     desc "Import latest #{bosh_release}"
     task :import, :path do |t, args|
+      target_bosh_release_path = File.expand_path("../bosh_release", __FILE__)
       source_bosh_release = File.expand_path(args[:path])
       unless File.directory?(source_bosh_release)
         $stderr.puts "Please pass path to source bosh release"
@@ -39,3 +49,11 @@ namespace :bosh do
 end
 
 task :release => "bosh:release:clean"
+
+desc "Run specs"
+unit_rspec_task = RSpec::Core::RakeTask.new(:unit) do |t|
+  t.pattern = "spec/**/*_spec.rb"
+  t.rspec_opts = %w(--format progress --color)
+end
+
+task :default => :spec
---
+++ spec/spec_helper.rb
@@ -0,0 +1,68 @@
+# Copyright (c) 2012-2013 Stark & Wayne, LLC
+
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
+
+require "rubygems"
+require "bundler"
+Bundler.setup(:default, :test)
+
+$:.unshift(File.expand_path("../../lib", __FILE__))
+
+require "rspec/core"
+require "tmpdir"
+require "bosh-bootstrap"
+require "bosh-bootstrap/cli/helpers"
+
+# for the #sh helper
+require "rake"
+require "rake/file_utils"
+
+# load all files in spec/support/* (but not lower down)
+Dir[File.dirname(__FILE__) + '/support/*'].each do |path|
+  require path unless File.directory?(path)
+end
+
+def spec_asset(filename)
+  File.expand_path("../assets/#{filename}", __FILE__)
+end
+
+def files_match(filename, expected_filename)
+  file = File.read(filename)
+  expected_file = File.read(expected_filename)
+  file.should == expected_file
+end
+
+def yaml_files_match(filename, expected_filename)
+  yaml = YAML.load_file(filename)
+  expected_yaml = YAML.load_file(expected_filename)
+  yaml.should == expected_yaml
+end
+
+def setup_home_dir
+  home_dir = File.expand_path("../../tmp/home", __FILE__)
+  FileUtils.rm_rf(home_dir)
+  FileUtils.mkdir_p(home_dir)
+  ENV['HOME'] = home_dir
+end
+
+# returns the file path to a file
+# in the fake $HOME folder
+def home_file(*path)
+  File.join(ENV['HOME'], *path)
+end
+
+RSpec.configure do |c|
+  c.before(:each) do
+    setup_home_dir
+  end
+
+  c.color_enabled = true
+end
+
+def get_tmp_file_path(content)
+  tmp_file = File.open(File.join(Dir.mktmpdir, "tmp"), "w")
+  tmp_file.write(content)
+  tmp_file.close
+
+  tmp_file.path
+end
