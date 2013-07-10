25a3bcff9f401f7e28b3c58f5ed07a85a7a71585
Dr Nic Williams <drnicwilliams@gmail.com>
2013-07-09 20:12:38 -0700

    initial bosh plugin stub

---
+++ lib/bosh/cli/commands/cf.rb
@@ -0,0 +1,23 @@
+require "yaml"
+
+# for the #sh helper
+require "rake"
+require "rake/file_utils"
+
+module Bosh::Cli::Command
+  class CloudFoundry < Base
+    include FileUtils
+    include Bosh::Cli::Validation
+
+    usage "cf"
+    desc  "show micro cf sub-commands"
+    def cf_help
+      say("bosh cf sub-commands:")
+      nl
+      cmds = Bosh::Cli::Config.commands.values.find_all {|c|
+        c.usage =~ /cf/
+      }
+      Bosh::Cli::Command::Help.list_commands(cmds)
+    end
+  end
+end
