2bd17d430d255ebf8dd642b0257dc8ebf6858a3e
Dr Nic Williams <drnicwilliams@gmail.com>
2013-07-19 23:02:38 -0600

    clean out cf_help spec

--- spec/commands/command_cf_help_spec.rb
+++ spec/commands/command_cf_help_spec.rb
@@ -3,11 +3,6 @@ require "bosh/cli/commands/99_cf_help"
 describe Bosh::Cli::Command::CloudFoundryHelp do
   let(:command) { Bosh::Cli::Command::CloudFoundryHelp.new }
 
-  before do
-    setup_home_dir
-    command.add_option(:config, home_file(".bosh_config"))
-  end
-
   it "shows help" do
     command.cf_help
   end
