94f990112a9c0ee227995be6a5e4f9dad3d9e64f
Dr Nic Williams <drnicwilliams@gmail.com>
2013-07-19 23:06:33 -0600

    Extract latest release number in spec into variable

--- spec/commands/command_prepare_cf_spec.rb
+++ spec/commands/command_prepare_cf_spec.rb
@@ -3,6 +3,7 @@ require "bosh/cli/commands/01_prepare_bosh_for_cf"
 describe Bosh::Cli::Command::PrepareBoshForCloudFoundry do
   let(:command) { Bosh::Cli::Command::PrepareBoshForCloudFoundry.new }
   let(:director) { instance_double("Bosh::Cli::Director") }
+  let(:latest_release_version_number) { 133 }
 
   before do
     setup_home_dir
@@ -18,25 +19,29 @@ describe Bosh::Cli::Command::PrepareBoshForCloudFoundry do
       command.stub(:director).and_return(director)
     end
 
-    context "director does not already have release" do
-      it "upload release" do
-        release_yml = File.expand_path("../../../bosh_release/releases/cf-release-133.yml", __FILE__)
-        release_cmd = instance_double("Bosh::Cli::Command::Release")
-        release_cmd.should_receive(:upload).with(release_yml)
-        command.stub(:release_cmd).and_return(release_cmd)
+    it "upload release if bosh does not already have release" do
+      release_yml = File.expand_path("../../../bosh_release/releases/cf-release-#{latest_release_version_number}.yml", __FILE__)
+      release_cmd = instance_double("Bosh::Cli::Command::Release")
+      release_cmd.should_receive(:upload).with(release_yml)
+      command.stub(:release_cmd).and_return(release_cmd)
 
-        aws_full_stemcell_url = "http://bosh-jenkins-artifacts.s3.amazonaws.com/bosh-stemcell/aws/latest-bosh-stemcell-aws.tgz"
-        stemcell_cmd = instance_double("Bosh::Cli::Command::Stemcell")
-        stemcell_cmd.should_receive(:upload).with(aws_full_stemcell_url)
-        command.stub(:stemcell_cmd).and_return(stemcell_cmd)
+      aws_full_stemcell_url = "http://bosh-jenkins-artifacts.s3.amazonaws.com/bosh-stemcell/aws/latest-bosh-stemcell-aws.tgz"
+      stemcell_cmd = instance_double("Bosh::Cli::Command::Stemcell")
+      stemcell_cmd.should_receive(:upload).with(aws_full_stemcell_url)
+      command.stub(:stemcell_cmd).and_return(stemcell_cmd)
 
-        command.prepare_cf
-      end
+      command.prepare_cf
     end
 
-    context "director already has release" do
-      it "do not upload"
-    end
+    it "upload specific release"
+
+    it "errors if specific requested release does not exist"
+
+    it "errors if specific requested release is not enabled"
+
+    it "do not upload release if bosh already has that release"
+
+    it "do not upload stemcell if bosh already has stemcell"
   end
 
 end
